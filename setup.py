a6a5d76e2b9b81cd143e6b6db2666def1fb02e80
James Tocknell <aragilar@gmail.com>
2014-01-24 18:03:19 +1100

    Added setup.py

---
+++ setup.py
@@ -0,0 +1,14 @@
+import setuptools
+
+setuptools.setup(
+        name="venv_tools",
+        version="0.1",
+        packages = "venv_tools",
+        install_requires = ["requests"]
+        author = "James Tocknell",
+        author_email = "aragilar@gmail.com",
+        description = "A bunch of tools for using venvs (and virtualenvs) from python",
+        license = "BSD",
+        keywords = "virtualenv venv",
+        url = ""
+)
