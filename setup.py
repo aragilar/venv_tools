e1d3efc6aa5179ce9abfd868ffb29fd350c6d9d9
James Tocknell <aragilar@gmail.com>
2014-03-23 15:25:10 +1100

    Name fix

--- setup.py
+++ setup.py
@@ -21,7 +21,7 @@ def find_version(*file_paths):
     if version_match:
         return version_match.group(1)
     raise RuntimeError("Unable to find version string.")
-with open('DESCRIPTION.rst', encoding='utf-8') as f:
+with open('README') as f:
     long_description = f.read()
 
 deps = ["requests"]
